296cbdc8b20778ee78dbd567ceeed27b8a40f064
Damien Hogan <damienh@users.noreply.github.com>
2020-06-08 14:20:08 +0100

    switch to using uuid instead of integers for primary keys and fix migrations (#9)

--- db/migrate/20200601135451_create_lessons.rb
+++ db/migrate/20200601135451_create_lessons.rb
@@ -1,9 +1,9 @@
 class CreateLessons < ActiveRecord::Migration[6.0]
   def change
-    create_table :lessons do |t|
+    create_table :lessons, id: :uuid do |t|
       t.string :title
       t.text :description
-      t.references :unit, null: false, foreign_key: true
+      t.belongs_to :unit, type: :uuid, null: false, index: true
 
       t.timestamps
     end
