5e354e78cb342fcb273fa8bf303dcfbe81b4de17
Damien Hogan <damienhogan87@gmail.com>
2020-06-09 09:11:18 +0100

    use sequence for creating YearGroups

--- spec/factories/year_group.rb
+++ spec/factories/year_group.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :year_group do
-    title { '9' }
-    description { 'Year 9 covers age 13.' }
+    sequence(:title, 1) { |n| "Year #{n}" }
+    sequence(:description, 1) { |n| "Year #{n} covers age #{n + 2}" }
     key_stage
   end
 end
