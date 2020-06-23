8080d20ca3397992bbf4b04707f2e9e04ba9b938
Ant Barnes <ant@arayb.com>
2020-06-23 16:30:08 +0100

    use same hook for update and create

--- app/controllers/concerns/publishable_controller.rb
+++ app/controllers/concerns/publishable_controller.rb
@@ -2,9 +2,8 @@ module PublishableController
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_publish_prop, only: %i[update]
     before_action :generate_id, only: %i[create]
-    after_action :publish_after_create, only: %i[create]
+    after_action :set_published_state, only: %i[create update]
   end
 
   def resource_params
@@ -15,20 +14,27 @@ module PublishableController
 
   private
 
-    def set_publish_prop
-      published_param = params.require(resource_class.model_name.param_key)['published']
-      requested_resource.publish = ActiveRecord::Type::Boolean.new.cast(published_param)
-    end
-
     def generate_id
       @new_id = SecureRandom.uuid
       params[resource_name][:id] = @new_id
     end
 
-    def publish_after_create
-      published_param = params.require(resource_class.model_name.param_key)['published']
+    def set_published_state
+      published_param = params.require(resource_class.model_name.param_key)[:published]
       publish = ActiveRecord::Type::Boolean.new.cast(published_param)
-      resource = resource_class.find_by(id: @new_id)
-      resource.published! if resource && publish
+
+      resource = if params[:action] == 'create'
+                   resource_class.find_by(id: @new_id)
+                 else
+                   requested_resource
+                 end
+
+      return unless resource
+
+      if publish
+        resource.published!
+      else
+        resource.unpublished!
+      end
     end
 end
