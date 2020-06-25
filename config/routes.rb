64d66160d031dd4fe09f1b27955d963dd908581a
Magdalena Jadach <magdalena@raspberrypi.org>
2020-06-25 12:11:40 +0100

    Play with routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'admin/key_stages#index'
+  root to: 'admin/application_controller#index'
+  #root to: 'admin/key_stages#index'
+	# root to: 'admin_root_path'
 
   namespace :admin do
     resources :units
