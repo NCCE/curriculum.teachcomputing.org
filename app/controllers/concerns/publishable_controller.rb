0ac860d9b3fb667c1006fd6122dd9f91f9999b95
Ant Barnes <ant@arayb.com>
2020-06-25 13:45:17 +0100

    Move publishable behaviour to show action
    Adds endpoints to publish/unpublish a model
    Adds buttons to show page for a publishable model allowing a record to be published or unpublished

--- app/controllers/concerns/publishable_controller.rb
+++ app/controllers/concerns/publishable_controller.rb
@@ -1,40 +1,28 @@
 module PublishableController
   extend ActiveSupport::Concern
 
-  included do
-    before_action :generate_id, only: %i[create]
-    after_action :set_published_state, only: %i[create update]
+  def publish
+    resource = fetch_resource
+    resource.published!
+    redirect_to_show(resource, 'publish.success')
   end
 
-  def resource_params
-    params.require(resource_class.model_name.param_key)
-          .permit(dashboard.permitted_attributes << :id)
-          .transform_values { |v| read_param_value(v) }
+  def unpublish
+    resource = fetch_resource
+    resource.unpublished!
+    redirect_to_show(resource, 'unpublish.success')
   end
 
   private
 
-    def generate_id
-      @new_id = SecureRandom.uuid
-      params[resource_name][:id] = @new_id
+    def fetch_resource
+      resource_class.find(params["#{resource_name}_id"])
     end
 
-    def set_published_state
-      published_param = params.require(resource_class.model_name.param_key)[:published]
-      publish = ActiveRecord::Type::Boolean.new.cast(published_param)
-
-      resource = if params[:action] == 'create'
-                   resource_class.find_by(id: @new_id)
-                 else
-                   requested_resource
-                 end
-
-      return unless resource
-
-      if publish
-        resource.published!
-      else
-        resource.unpublished!
-      end
+    def redirect_to_show(resource, message)
+      redirect_to(
+        [namespace, resource],
+        notice: translate_with_resource(message)
+      )
     end
 end
