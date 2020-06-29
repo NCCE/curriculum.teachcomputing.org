3b9c0820290c5679121980394d3b4fd7af126a6b
Damien Hogan <damienhogan87@gmail.com>
2020-06-29 09:47:57 +0100

    tweaks

--- app/graphql/types/key_stage_type.rb
+++ app/graphql/types/key_stage_type.rb
@@ -3,7 +3,7 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :short_title, String, null: false
-    field :ks_number, Integer, null: false
+    field :ks_number, String, null: false
     field :description, String, null: false
     field :year_groups, [Types::YearGroupType], null: true
   end
--- spec/factories/key_stage.rb
+++ spec/factories/key_stage.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :key_stage do
-    sequence(:ks_number)
+    sequence(:ks_number, &:to_s)
     sequence(:description) { |n| "Key Stage 3, or KS3, is the part taught to children between the ages of 11 and 14. KS3 begins when pupils start secondary education - #{n}" }
     state
 
