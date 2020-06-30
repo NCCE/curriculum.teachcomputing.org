88cd5cc48fc15106e69697b3bf5b3c06d5cc0173
Damien Hogan <damienh@users.noreply.github.com>
2020-06-30 13:05:44 +0100

    Add UnitOverview to GrapQL (#48)
    * Add UnitOverview to GrapQL
    
    * update spec
    
    * push spec
    
    * update spec

--- app/graphql/types/unit_type.rb
+++ app/graphql/types/unit_type.rb
@@ -4,6 +4,11 @@ module Types
     field :title, String, null: false
     field :description, String, null: false
     field :assessments, [Types::AssessmentType], null: true
+    field :unit_overview, String, null: true
     field :lessons, [Types::LessonType], null: true
+
+    def unit_overview
+      url_for(object.unit_overview) if object.unit_overview.attachment
+    end
   end
 end
