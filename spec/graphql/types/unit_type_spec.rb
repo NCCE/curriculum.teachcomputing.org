baf00b91c49a6d83f5cdc4703d8323d87f44567f
Damien Hogan <damienhogan87@gmail.com>
2020-06-30 12:38:53 +0100

    update spec

--- spec/graphql/types/unit_type_spec.rb
+++ spec/graphql/types/unit_type_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Types::UnitType do
   it { is_expected.to have_field(:id).of_type('ID!') }
   it { is_expected.to have_field(:title).of_type('String!') }
   it { is_expected.to have_field(:description).of_type('String!') }
-  it { is_expected.to have_field(:unit_overview).of_type('String!') }
+  it { is_expected.to have_field(:unit_overview).of_type('String') }
   it { is_expected.to have_field(:assessments).of_type('[Assessment!]') }
   it { is_expected.to have_field(:lessons).of_type('[Lesson!]') }
 end
