6f2bee8ca9000f261768b6e798094c4d7347b56e
Dan Halson <danhalson@users.noreply.github.com>
2020-06-30 14:26:06 +0100

    Add missing fields and a rake task to allow generating schema (#47)

--- app/graphql/types/assessment_type.rb
+++ app/graphql/types/assessment_type.rb
@@ -1,6 +1,7 @@
 module Types
   class AssessmentType < BaseObject
     field :id, ID, null: false
+    field :unit, String, null: false
     field :title, String, null: false
     field :description, String, null: false
   end
--- app/graphql/types/year_group_type.rb
+++ app/graphql/types/year_group_type.rb
@@ -3,5 +3,7 @@ module Types
     field :id, ID, null: false
     field :year_number, String, null: false
     field :description, String, null: false
+    field :key_stage, ID, null: false
+    field :units, [Types::UnitType], null: true
   end
 end
