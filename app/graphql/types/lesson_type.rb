7f0d88f77dcbf05e9791fdf406e6d70529c83705
Dan Halson <dan.halson@raspberrypi.org>
2020-06-30 16:04:04 +0100

    Add field

--- app/graphql/types/lesson_type.rb
+++ app/graphql/types/lesson_type.rb
@@ -1,6 +1,7 @@
 module Types
   class LessonType < BaseObject
     field :id, ID, null: false
+    field :unit, String, null: false
     field :title, String, null: false
     field :description, String, null: false
   end
