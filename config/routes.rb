4244629dc6f1f29a50dbe97c9dd5f72e80736acb
Magdalena Jadach <magdalena@raspberrypi.org>
2020-06-30 10:45:18 +0100

    Update Admin route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'admin/application_controller#authenticate_admin'
-  #root to: 'admin/key_stages#index'
+  root to: redirect('/admin')
 
   namespace :admin do
     resources :units do
