c9ae06ef7dbdfede15059f0858a17c871cdd5837
Damien Hogan <damienhogan87@gmail.com>
2020-07-01 10:31:54 +0100

    Add Learning Graph to YearGroup

--- app/graphql/types/year_group_type.rb
+++ app/graphql/types/year_group_type.rb
@@ -3,7 +3,12 @@ module Types
     field :id, ID, null: false
     field :year_number, String, null: false
     field :description, String, null: false
+    field :learning_graph, String, null: true
     field :key_stage, ID, null: false
     field :units, [Types::UnitType], null: true
+
+    def learning_graph
+      url_for(object.learning_graph) if object.learning_graph.attachment
+    end
   end
 end
