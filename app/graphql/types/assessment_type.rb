a8610d8bf7d6d59fe9f4fd966baefc5347c7be3f
Damien Hogan <damienhogan87@gmail.com>
2020-07-01 10:34:07 +0100

    Add active storage fields to assessment

--- app/graphql/types/assessment_type.rb
+++ app/graphql/types/assessment_type.rb
@@ -4,5 +4,20 @@ module Types
     field :unit, String, null: false
     field :title, String, null: false
     field :description, String, null: false
+    field :rubric, String, null: true
+    field :sheets, [String], null: true
+    field :summative_assessment, String, null: true
+
+    def rubric
+      url_for(object.rubric) if object.rubric.attachment
+    end
+
+    def sheets
+      object.sheets.map { |record| url_for(record) } if object.sheets.attachments
+    end
+
+    def summative_assessment
+      rl_for(object.summative_assessment) if object.summative_assessment.attachment
+    end
   end
 end
