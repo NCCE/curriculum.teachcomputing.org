5846db6de1bf688e6af112cef5b53dbe6ea24775
Damien Hogan <damienhogan87@gmail.com>
2020-07-01 10:28:21 +0100

    Add active storage fields to LessonType

--- app/graphql/types/lesson_type.rb
+++ app/graphql/types/lesson_type.rb
@@ -3,5 +3,15 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :description, String, null: false
+    field :lesson_plan, String, null: true
+    field :activities, [String], null: true
+
+    def lesson_plan
+      url_for(object.lesson_plan) if object.lesson_plan.attachment
+    end
+
+    def activities
+      object.activities.map { |record| url_for(record) } if object.activities.attachments
+    end
   end
 end
