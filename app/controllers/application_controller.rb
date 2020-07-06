49052513b21b2d34dc7d0deae83980bcd2522650
Ant Barnes <ant@arayb.com>
2020-07-06 14:30:18 +0100

    Add token auth check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  private
+
+    def authenticate
+      return true if Rails.env.development? || Rails.env.test?
+
+      authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, _options|
+        token == ENV['API_KEY']
+      end
+    end
+
+    def render_unauthorized
+      Raven.capture_message('Unauthorized access attempt')
+      headers['WWW-Authenticate'] = 'Token realm="Application"'
+      render json: 'Bad credentials', status: 401
+    end
 end
