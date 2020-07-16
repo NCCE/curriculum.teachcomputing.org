1b74d8dfad7558bffff67b630e5c228b1e59f84d
Damien Hogan <damienhogan87@gmail.com>
2020-07-16 16:00:04 +0100

    Set scope for Lesson

--- app/graphql/types/unit_type.rb
+++ app/graphql/types/unit_type.rb
@@ -6,12 +6,16 @@ module Types
     field :title, String, null: false
     field :description, String, null: false
     field :unit_guide, String, null: true, method: :unit_guide_url
-    field :lessons, [Types::LessonType], null: true, method: :published_lessons
+    field :lessons, [Types::LessonType], null: true
     field :total_positive, Integer, null: true
     field :total_negative, Integer, null: true
     field :learning_graphs, [String], null: true, method: :learning_graphs_urls
     field :rubrics, [String], null: true, method: :rubrics_urls
     field :summative_assessments, [String], null: true, method: :summative_assessments_urls
     field :summative_answers, [String], null: true, method: :summative_answers_urls
+
+    def lessons
+      object.lessons.published.ordered
+    end
   end
 end
