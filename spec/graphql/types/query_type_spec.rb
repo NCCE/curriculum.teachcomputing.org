7b0a876abe05a13bf11a5faa79a440ed41969cb8
Damien Hogan <damienh@users.noreply.github.com>
2020-07-16 11:24:23 +0100

    update to query type (#72)
    * update to query type
    
    * update spec

--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Types::QueryType do
   subject { described_class }
 
-  it { is_expected.to have_field(:year_groups).of_type('[YearGroup!]!') }
-  it { is_expected.to have_field(:key_stages).of_type('[KeyStage!]!') }
-  it { is_expected.to have_field(:lessons).of_type('[Lesson!]!') }
-  it { is_expected.to have_field(:units).of_type('[Unit!]!') }
+  it { is_expected.to have_field(:year_groups).of_type('[YearGroup!]') }
+  it { is_expected.to have_field(:key_stages).of_type('[KeyStage!]') }
+  it { is_expected.to have_field(:lessons).of_type('[Lesson!]') }
+  it { is_expected.to have_field(:units).of_type('[Unit!]') }
 
   describe 'year_group' do
     subject { described_class.fields['yearGroup'] }
 
-    it { is_expected.to be_of_type('YearGroup!') }
+    it { is_expected.to be_of_type('YearGroup') }
     it { is_expected.to accept_argument(:id).of_type('ID') }
     it { is_expected.to accept_argument(:slug).of_type('String') }
   end
@@ -19,7 +19,7 @@ RSpec.describe Types::QueryType do
   describe 'key_stage' do
     subject { described_class.fields['keyStage'] }
 
-    it { is_expected.to be_of_type('KeyStage!') }
+    it { is_expected.to be_of_type('KeyStage') }
     it { is_expected.to accept_argument(:id).of_type('ID') }
     it { is_expected.to accept_argument(:slug).of_type('String') }
   end
@@ -27,7 +27,7 @@ RSpec.describe Types::QueryType do
   describe 'lesson' do
     subject { described_class.fields['lesson'] }
 
-    it { is_expected.to be_of_type('Lesson!') }
+    it { is_expected.to be_of_type('Lesson') }
     it { is_expected.to accept_argument(:id).of_type('ID') }
     it { is_expected.to accept_argument(:slug).of_type('String') }
   end
@@ -35,7 +35,7 @@ RSpec.describe Types::QueryType do
   describe 'unit' do
     subject { described_class.fields['unit'] }
 
-    it { is_expected.to be_of_type('Unit!') }
+    it { is_expected.to be_of_type('Unit') }
     it { is_expected.to accept_argument(:id).of_type('ID') }
     it { is_expected.to accept_argument(:slug).of_type('String') }
   end
