1dff08f140614eb6288115b9b6cabc2af0bec0fb
Damien Hogan <damienhogan87@gmail.com>
2020-07-21 09:12:39 +0100

    schema back

---
+++ db/schema.rb
@@ -0,0 +1,121 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_17_103920) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.uuid "record_id", null: false
+    t.string "record_type", null: false
+    t.uuid "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "aggregate_ratings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.integer "total_positive", default: 0, null: false
+    t.integer "total_negative", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "key_stages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "state_id"
+    t.string "level"
+    t.string "ages"
+    t.string "years"
+    t.string "slug"
+    t.index ["slug"], name: "index_key_stages_on_slug", unique: true
+  end
+
+  create_table "lessons", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.uuid "unit_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "state_id"
+    t.uuid "aggregate_rating_id"
+    t.string "slug"
+    t.text "objectives"
+    t.boolean "zip_synced", default: false
+    t.datetime "zip_synced_at"
+    t.index ["slug"], name: "index_lessons_on_slug", unique: true
+    t.index ["unit_id"], name: "index_lessons_on_unit_id"
+  end
+
+  create_table "ratings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "aggregate_rating_id", null: false
+    t.boolean "positive", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["aggregate_rating_id"], name: "index_ratings_on_aggregate_rating_id"
+  end
+
+  create_table "states", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.integer "state", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "units", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.uuid "year_group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "state_id"
+    t.uuid "aggregate_rating_id"
+    t.string "slug"
+    t.boolean "unit_documents_synced", default: false
+    t.datetime "unit_documents_synced_at"
+    t.index ["slug"], name: "index_units_on_slug", unique: true
+    t.index ["year_group_id"], name: "index_units_on_year_group_id"
+  end
+
+  create_table "year_groups", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "key_stage_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "state_id"
+    t.string "year_number"
+    t.string "slug"
+    t.index ["key_stage_id"], name: "index_year_groups_on_key_stage_id"
+    t.index ["slug"], name: "index_year_groups_on_slug", unique: true
+  end
+
+  add_foreign_key "key_stages", "states"
+  add_foreign_key "lessons", "aggregate_ratings"
+  add_foreign_key "lessons", "states"
+  add_foreign_key "ratings", "aggregate_ratings"
+  add_foreign_key "units", "aggregate_ratings"
+  add_foreign_key "units", "states"
+  add_foreign_key "year_groups", "states"
+end
