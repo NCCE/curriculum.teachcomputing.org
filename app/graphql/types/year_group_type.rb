427b8761b9655cd2413ea9e0f85012475966f09b
Damien Hogan <damienhogan87@gmail.com>
2020-07-21 08:30:45 +0100

    remove description

--- app/graphql/types/year_group_type.rb
+++ app/graphql/types/year_group_type.rb
@@ -4,7 +4,6 @@ module Types
     field :slug, String, null: false
     field :key_stage, Types::KeyStageType, null: false
     field :year_number, String, null: false
-    field :description, String, null: false
     field :units, [Types::UnitType], null: true
 
     def units
