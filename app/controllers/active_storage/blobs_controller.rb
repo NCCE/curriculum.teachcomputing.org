c9b53182c0646591ced98beaafbea8d8e20f6f6a
Ant Barnes <e1daf3a5@opayq.com>
2020-07-27 16:17:49 +0100

    Override blobs controller to allow extending behaviour (#89)

---
+++ app/controllers/active_storage/blobs_controller.rb
@@ -0,0 +1,13 @@
+class ActiveStorage::BlobsController < ActiveStorage::BaseController
+  include ActiveStorage::SetBlob
+  after_action :track_download
+
+  def show
+    expires_in ActiveStorage.service_urls_expire_in
+    redirect_to @blob.service_url(disposition: params[:disposition])
+  end
+
+  private
+
+    def track_download; end
+end
