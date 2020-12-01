750803c4961ee445975547309e2c4c74c65a7c22
Dan Halson <dan.halson@raspberrypi.org>
2020-12-01 11:58:13 +0000

    1504: Remove unecessary aggregate_download relation from map model

--- app/models/curriculum_map.rb
+++ app/models/curriculum_map.rb
@@ -1,7 +1,6 @@
 class CurriculumMap < ApplicationRecord
   belongs_to :key_stage
 
-  has_many :aggregate_downloads, as: :downloadable, dependent: :destroy
   has_one_attached :file
 
   validates :name, :file, presence: true
