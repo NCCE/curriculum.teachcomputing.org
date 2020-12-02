f6091a02f9f49bdd6b95f9fd6a3231409131fb41
Dan Halson <dan.halson@raspberrypi.org>
2020-12-02 17:12:02 +0000

    Reinstate unless, and add datadog vars to app.json

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,3 +1,3 @@
 Datadog.configure do |c|
-  c.use :rails, service_name: "curriculum_teachcomputing_#{Rails.env}"
+  c.use :rails, service_name: "curriculum_teachcomputing_#{Rails.env}" unless Rails.env.development?
 end
