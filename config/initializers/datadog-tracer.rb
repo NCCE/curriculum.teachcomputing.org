bde232e2935b1a4d1a922b64724155aba62543a3
Dan Halson <dan.halson@raspberrypi.org>
2020-12-02 14:31:12 +0000

    Attempt to fix build failure

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -1,3 +1,3 @@
 Datadog.configure do |c|
-  c.use :rails, service_name: "curriculum_teachcomputing_#{Rails.env}" unless Rails.env.development?
+  c.use :rails, service_name: "curriculum_teachcomputing_#{Rails.env}" unless ENV['RAILS_ENV'] == 'development'
 end
