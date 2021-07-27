cc0af267b474ce451d864da9541da889fe58b410
Ant Barnes <e1daf3a5@opayq.com>
2021-07-27 09:32:59 +0100

    Make updating of learning objectives work (#161)

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     end
     resources :curriculum_maps, except: :index
     resources :taxonomy_tags
-    resources :learning_objectives, only: %i[index show edit]
+    resources :learning_objectives, only: %i[index show edit update]
     resources :success_criteria, only: %i[show]
     root to: 'units#index'
   end
