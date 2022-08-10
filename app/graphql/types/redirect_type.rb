ecb0b8bda630fe9ce1a84be34e8bbe9bcc41a892
Dan Halson <dan.halson@raspberrypi.org>
2022-08-10 16:02:10 +0100

    Add to_context field, to ensure redirects work across units

--- app/graphql/types/redirect_type.rb
+++ app/graphql/types/redirect_type.rb
@@ -3,5 +3,14 @@ module Types
     field :from, String, null: false
     field :from_context, String, null: false
     field :to, String, null: false
+
+    field :to_context, String, null: false
+
+    def to_context
+      type = object.redirectable_type
+      redirectable = type.constantize.find(object.redirectable_id)
+
+      type == 'Unit' ? redirectable.key_stage.slug : redirectable.unit.slug
+    end
   end
 end
