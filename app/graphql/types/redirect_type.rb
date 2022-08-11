59232f99feb30541ad54aee8e5412bdc7b18b74c
Dan Halson <dan.halson@raspberrypi.org>
2022-08-11 11:12:37 +0100

    Further tweak to support key stage on lesson

--- app/graphql/types/redirect_type.rb
+++ app/graphql/types/redirect_type.rb
@@ -4,13 +4,13 @@ module Types
     field :from_context, String, null: false
     field :to, String, null: false
 
-    field :to_context, String, null: false
+    field :to_context, [String], null: false
 
     def to_context
       type = object.redirectable_type
       redirectable = type.constantize.find(object.redirectable_id)
 
-      type == 'Unit' ? redirectable.key_stage.slug : redirectable.unit.slug
+      type == 'Unit' ? [redirectable.key_stage.slug] : [redirectable.unit.key_stage.slug, redirectable.unit.slug]
     end
   end
 end
