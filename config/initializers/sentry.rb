2ffe109fdb4e7fd581a3858308ebcb97f76468b4
timregan <tim_regan82@hotmail.com>
2022-10-06 15:12:29 +0100

    Swap to ENV.fetch

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = Rails.application.credentials[ENV.fetch('SENTRY_DSN')]
+  config.dsn = ENV.fetch('SENTRY_DSN')
   config.breadcrumbs_logger = [:active_support_logger]
   config.traces_sample_rate = 0.25
   config.enabled_environments = %w[staging production]
