b4eee244d3bf7ea743f898acde92827968443fa8
Michael Squance <m.squance@stem.org.uk>
2024-01-15 11:18:27 +0000

    Applying standardrb fixes to spec tests

--- spec/requests/admin/key_stages_spec.rb
+++ spec/requests/admin/key_stages_spec.rb
@@ -1,29 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Admin::KeyStagesController' do
-
+RSpec.describe "Admin::KeyStagesController" do
   let(:key_stage) { create(:key_stage) }
 
-  describe 'GET #index' do
-
+  describe "GET #index" do
     before do
       get admin_key_stages_path
     end
 
-    it 'renders the correct tempate' do
-      expect(response).to render_template('index')
+    it "renders the correct tempate" do
+      expect(response).to render_template("index")
     end
   end
 
-  describe 'GET #show' do
-
+  describe "GET #show" do
     before do
       get admin_key_stage_path(key_stage)
     end
 
-    it 'render the correct template' do
-      expect(response).to render_template('show')
+    it "render the correct template" do
+      expect(response).to render_template("show")
     end
-
   end
 end
--- spec/requests/admin/units_spec.rb
+++ spec/requests/admin/units_spec.rb
@@ -1,29 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Admin::UnitsController' do
+RSpec.describe "Admin::UnitsController" do
   let(:unit) { create(:unit) }
 
-  describe 'GET #index' do
-
+  describe "GET #index" do
     before do
       get admin_units_path
     end
 
-    it 'renders the correct tempate' do
-      expect(response).to render_template('index')
+    it "renders the correct tempate" do
+      expect(response).to render_template("index")
     end
   end
 
-  describe 'GET #show' do
-
+  describe "GET #show" do
     before do
       get admin_unit_path(unit)
     end
 
-    it 'render the correct template' do
-      expect(response).to render_template('show')
+    it "render the correct template" do
+      expect(response).to render_template("show")
     end
-
   end
-
 end
