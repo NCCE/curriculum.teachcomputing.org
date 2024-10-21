010489467fd801a17a0114c58a84b2f8568f019b
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-21 14:25:00 +0000

    Fixed typo in types in url_links

--- spec/graphql/types/url_link_type_spec.rb
+++ spec/graphql/types/url_link_type_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe Types::UrlLinkType do
 
   it { is_expected.to have_field(:id).of_type("ID!") }
   it { is_expected.to have_field(:label).of_type("String!") }
-  it { is_expected.to have_field(:url).of_type("String") }
+  it { is_expected.to have_field(:url).of_type("String!") }
 end
