10752822fd2d6e88fd7554d6ba4a4e2536a39b42
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-22 12:44:02 +0000

    Merged all db changes into one migration

--- db/migrate/20241021102938_create_url_links.rb
+++ db/migrate/20241021102938_create_url_links.rb
@@ -1,8 +1,9 @@
 class CreateUrlLinks < ActiveRecord::Migration[6.1]
   def change
     create_table :url_links, id: :uuid do |t|
-      t.string :label
-      t.string :url
+      t.string :label, null: false
+      t.string :url, null: false
+      t.string :type_descriptor, null: true
       t.references :unit, type: :uuid, foreign_key: true
 
       t.timestamps
--- db/migrate/20241021113348_make_fields_required_in_url_links.rb
+++
@@ -1,6 +0,0 @@
-class MakeFieldsRequiredInUrlLinks < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null(:url_links, :label, false)
-    change_column_null(:url_links, :url, false)
-  end
-end
