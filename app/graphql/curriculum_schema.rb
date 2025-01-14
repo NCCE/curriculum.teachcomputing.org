bc93f461c31f89c28a702c60a30f372c4bcc477c
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-14 12:28:38 +0000

    Removing graphql schema visibility

--- app/graphql/curriculum_schema.rb
+++ app/graphql/curriculum_schema.rb
@@ -2,8 +2,6 @@ class CurriculumSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  use GraphQL::Schema::Visibility
-
   rescue_from(ActiveRecord::RecordNotFound) do |err, obj, args, ctx, field|
     raise GraphQL::ExecutionError.new("#{field.type.unwrap.graphql_name} not found", extensions: {"code" => :not_found})
   end
