68eeb0b8cdd886b4a36ed64f219dd5fbeec47d68
Adam Wheatley <adamwheatley7@gmail.com>
2025-01-14 13:08:51 +0000

    Adding testing in for aggregate download in redirect spec

--- spec/requests/active_storage/blobs/redirect_spec.rb
+++ spec/requests/active_storage/blobs/redirect_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 RSpec.describe "Redirect", type: :request do
   let(:unit_with_attachment) { create(:published_unit) }
-
-  before do
-    get url_for(unit_with_attachment.unit_guide.attachment)
-  end
+  let(:user_stem_achiever_contact_no) { SecureRandom.uuid }
 
   it "should redirect to blob url" do
+    get url_for(unit_with_attachment.unit_guide.attachment)
     expect(response.status).to eq(302)
+  end
+
+  it "should call AggregateDownload" do
+    expect(AggregateDownload).to receive(:increment_attachment_download)
+      .with(unit_with_attachment.unit_guide.attachment, user_stem_achiever_contact_no)
+    get url_for(unit_with_attachment.unit_guide.attachment), params: {user_stem_achiever_contact_no:}
   end
 end
